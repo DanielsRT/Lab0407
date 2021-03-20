6bb9a35489fae0ced0386bce81da86e2a07aacda
DanielsRT <robdaniels2001@gmail.com>
2021-03-19 19:58:11 -0500

    first

--- Lab0324/Lab0324.java
+++ Lab0324/Lab0324.java
@@ -3,6 +3,6 @@ package Lab0324;
 public class Lab0324 {
     public static void main(String[] args) {
         System.out.println("Welcome to Lab 0324!");
-        System.out.println("Have I figured this out?");
+        
     }
 }
